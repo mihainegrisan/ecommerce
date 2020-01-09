d99a29cb8e5406b32172510ab20ff8466efb0cd1
mihai.negrisan <mihainegrisan1@gmail.com>
2020-01-09 20:02:10 +0200

    urls changed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
 if settings.DEBUG: # or settings.TESTING_MODE for development
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
