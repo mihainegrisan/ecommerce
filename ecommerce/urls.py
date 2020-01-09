b10bd22069334214caf00f106b4dacf4090f32f5
mihai.negrisan <mihainegrisan1@gmail.com>
2020-01-09 16:03:56 +0200

    Solved testing_mode error

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG or settings.TESTING_MODE:
+if settings.DEBUG: # or settings.TESTING_MODE for development
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
