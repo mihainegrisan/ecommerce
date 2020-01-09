be56e73dad096f339114dc499bd5760fd92c31c7
mihai.negrisan <mihainegrisan1@gmail.com>
2020-01-09 10:15:03 +0200

    Added Procfile

--- src/ecommerce/settings/base.py
+++ src/ecommerce/settings/base.py
@@ -82,7 +82,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
